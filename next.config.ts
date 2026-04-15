dd3ac52fbc4ca42b5d69b0a90a541fae6b845619
kavindi03 <it23655966@my.sliit.lk>
2026-04-15 15:39:31 +0530

    Fix Netlify - add images unoptimized for static export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
